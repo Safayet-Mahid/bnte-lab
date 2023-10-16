27a15c7356e14fd9642487250b46c74e03153a2a
Safayet-Mahid <safayetmahid96@gmail.com>
2023-10-16 18:48:57 +0600

    all header footer affiliation made common

--- js/components.js
+++ js/components.js
@@ -22,104 +22,44 @@ class Footer extends HTMLElement {
       href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css"
       rel="stylesheet"
     />  
-    <footer>
-    <section
-      class="lg:px-52 sm:px-10 bg-neutral-100 text-stone-600 py-10 grid sm:grid-cols-1 lg:grid-cols-5 lg:gap-14 sm:gap-5"
-    >
-      <div
-        class="flex flex-col lg:flex-row lg:items-start gap-2 lg:col-span-2"
-      >
-        <p class="text-2xl"><i class="fa-solid fa-location-dot"></i></p>
-        <a class="" href=""
-          >Main Office and Lab
-          <span class="text-xs block"
-            >622 West 168th Street, Vanderbilt Clinic, Room 12-234 MC 104B ·
-            New York, NY 10032</span
-          ></a
-        >
-      </div>
-      <div class="flex flex-col lg:flex-row lg:items-start gap-2">
-        <p class="text-2xl"><i class="fa-solid fa-envelope"></i></p>
-        <a class="" href=""
-          >Contact Us
-          <span
-            class="underline text-cyan-600 font-medium lg:text-sm text-[0.65rem] block"
-            >gvnlab@gmail.com</span
-          ></a
-        >
-      </div>
-      <div class="space-x-2">
-        <p>Follow Us</p>
-        <p class="text-2xl text-sky-600">
-          <i class="fa-brands fa-twitter"></i>
-        </p>
-      </div>
-      <div class="flex flex-col gap-y-1 text-xs lg:text-base">
-        <a class="underline text-sky-700 font-medium" href="pages/research.html"
-          >Research</a
-        >
-        <a
-          class="underline text-sky-700 font-medium"
-          href="pages/publications.html"
-          >Publications</a
-        >
-        <a class="underline text-sky-700 font-medium" href="pages/people.html"
-          >People</a
-        >
-        <a class="underline text-sky-700 font-medium" href="pages/news.html"
-          >News</a
-        >
-        <a class="underline text-sky-700 font-medium" href="pages/contact.html"
-          >Contact</a
-        >
-        <a class="underline text-sky-700 font-medium" href="pages/positions.html"
-          >Positions</a
-        >
-      </div>
-    </section>
-    <section
-      class="lg:px-52 px-10 bg-sky-800 py-10 flex sm:flex-col lg:flex-row sm:justify-center items-center lg:justify-between"
-    >
-      <div class="flex items-start gap-x-2">
-        <p class="text-white lg:text-2xl">
-          <i class="fa-solid fa-crown"></i>
-        </p>
-        <a class="text-white lg:text-2xl font-serif" href=""
-          >Columbia University
-          <span class="lg:lg:text-sm text-[0.65rem] text-[0.65rem] block"
-            >In the city of New York</span
-          ></a
-        >
-      </div>
-      <div class="space-x-3 flex flex-wrap">
-        <a
-          class="underline text-white lg:text-sm text-[0.65rem]"
-          href="http://"
-          >©2023 Columbia University</a
-        >
-        <a
-          class="underline text-white lg:text-sm text-[0.65rem]"
-          href="http://"
-          >Accessibility</a
-        >
-        <a
-          class="underline text-white lg:text-sm text-[0.65rem]"
-          href="http://"
-          >Nondiscrimination</a
-        >
-        <a
-          class="underline text-white lg:text-sm text-[0.65rem]"
-          href="http://"
-          >Careers</a
-        >
-        <a
-          class="underline text-white lg:text-sm text-[0.65rem]"
-          href="http://"
-          >Built using Columbia Sites</a
-        >
-      </div>
-    </section>
-  </footer>    
+    <footer class="space-y-6 lg:space-y-16">
+        <section
+            class="lg:px-52 px-5 bg-neutral-100 text-stone-600 py-10 grid lg:grid-cols-4 grid-cols-1 lg:gap-14 gap-5">
+            <div class="flex flex-col lg:flex-row lg:items-start gap-2 lg:col-span-2">
+              <p class="text-2xl"><i class="fa-solid fa-location-dot"></i></p>
+              <a class="" href="">Address
+              <span class="text-xs block">Department of Applied Chemistry and Chemical Engineering (ACCE), <br>
+              Faculty of Science,<br>
+              University of Chittagong, <br>
+              Chittagong-4331, Bangladesh</span></a>
+            </div>
+            <div class="flex flex-col lg:flex-row lg:items-start gap-2 ">
+              <p class="text-2xl"><i class="fa-solid fa-envelope"></i></p>
+              <a class="" href="">Contact Us
+                <span
+                  class="underline text-yellow-600 font-medium lg:text-sm text-[0.65rem] block">bntelabacce.cu@gmail.com</span></a>
+            </div>
+            <div class="space-x-2 ">
+              <p>Follow Us</p>
+              <p class="text-2xl text-sky-600">
+                <i class="fa-brands fa-twitter"></i>
+              </p>
+            </div>
+          </section>
+
+          <section
+            class="lg:px-52 px-10 bg-yellow-600 py-10 flex flex-col lg:flex-row justify-center items-center lg:justify-between">
+            <div class="flex items-center gap-x-2">
+              <img src="./images/cu-logo-black.svg" class="w-6 h-6">
+              <a class="text-white lg:text-2xl " href="https://cu.ac.bd/" target="_blank">University of Chittagong</a>
+            </div>
+            <div class="space-x-3 flex flex-wrap">
+              <span class=" text-white lg:text-sm text-[0.65rem]">©2023 Bio-Nanomaterials and Tissue Engineering Laboratory
+                (BNTELab)</span>
+            </div>
+          </section>
+    
+    </footer>    
       `;
   }
 }
